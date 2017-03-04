31926d83e7f77491c95e060829ad65fa0009c855
Kavya Sukumar <kavyasukumar@users.noreply.github.com>
2017-03-03 22:51:38 -0500

    Update pie.js

--- pie.js
+++ pie.js
@@ -2,9 +2,9 @@ var dataText = thisComp.layer("data").text.sourceText.value;
 var data = eval(dataText);
 var dataIndex = thisLayer.index - thisComp.layer("data").index - 1;
 var w = 0;
-for(var i=0; i<= dataIndex; i++){
-	w = w+ data[i];
+for(var i = 0; i <= dataIndex; i++){
+	w = w + data[i];
 }
 
-[linear(time, 0,2, 0, w), 100];
+[linear(time, 0, 2, 0, w), 100];
 
